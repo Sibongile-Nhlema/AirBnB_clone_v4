9c444b3b27d5f798c0446c8a3588fb8013e44939
Sibongile <sibongile.nhlema@gmail.com>
2024-04-05 09:39:11 +0200

    Implement frontend rendering of places data

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -12,16 +12,38 @@ $(document).ready(function () {
     $('.amenities h4').text(amenitiesList);
   });
 
-  $.post('http://0.0.0.0:5001/api/v1/places_search/', JSON.stringify({}), (data) => {
-    const sectionPlaces = $('section.places');
 
-    data.forEach(place => {
-      const article = `<article><div class="title_box"><h2>${place.name}</h2><div class="price_by_night">$${place.price_by_night}</div></div><div class="information"><div class="max_guest">${place.max_guest} Guests</div><div class="number_rooms">${place.number_rooms} Bedrooms</div><div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div></div><div class="description">${place.description}</div></article>`;
-      sectionPlaces.append(article);
+  $(document).ready(function () {
+    $.ajax({
+        type: 'POST',
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function (data) {
+            const sectionPlaces = $('section.places');
+
+            data.forEach(place => {
+                const article = `<article>
+                                    <div class="title_box">
+                                        <h2>${place.name}</h2>
+                                        <div class="price_by_night">$${place.price_by_night}</div>
+                                    </div>
+                                    <div class="information">
+                                        <div class="max_guest">${place.max_guest} Guests</div>
+                                        <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                                        <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                                    </div>
+                                    <div class="description">${place.description}</div>
+                                </article>`;
+                sectionPlaces.append(article);
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error('Error fetching places:', error);
+        }
     });
-  }).fail((error) => {
-    console.error('Error fetching places:', error);
-  });
+});
+
 
   $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
     const apiStatus = $('div#api_status');
